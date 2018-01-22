35cec89e710709c858142d615a968f278cb9143b
Pawinee Pinta <pawineep58@email.nu.ac.th>
2018-01-22 22:35:35 +0700

    firt commit

--- client.py
+++ client.py
@@ -1,13 +1,17 @@
 import socket
 
 UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
-MESSAGE = "Hello, World!"
+UDP_PORT = 5011
+##MESSAGE = "Hello, World!"
+NUMBER = "5"
 
 print "UDP target IP:", UDP_IP
 print "UDP target port:", UDP_PORT
-print "message:", MESSAGE
+print "message:", NUMBER
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+sock.sendto(NUMBER, (UDP_IP, UDP_PORT))
+while True:
+    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    print "factorial:", data
--- server.py
+++ server.py
@@ -1,9 +1,18 @@
 import socket
 UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
+UDP_PORT = 5010
+Sum = 1
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
+def factorial(num):
+    if num==0:
+        return 1
+    else:
+        return num * factorial(num-1)
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print "received message:", data
+    print "factorial:", factorial(int(data))
+    f = str (factorial(int(data)))
+    sock.sendto(f,addr)
+    
